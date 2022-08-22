4f67258aa14f871f24731e178147583995cfa6b5
Aclaputra <muhamadacla22@gmail.com>
2022-08-22 16:58:40 +0700

    feat: added create. update. delete function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,5 +18,41 @@ class ProductController extends Controller {
         $product = Product::find($id);
         return response()->json($prodcut);
     }
-    
+    /**
+     * create new product.
+     */
+    public function create(Request $request) {
+        $product = new Product();
+
+        $product->name      = $request->name;
+        $product->price     = $request->price;
+        $product->category  = $request->category;
+
+        $product->save();
+
+        return response()->json("Product Successfully Created!");
+    }
+    /**
+     * update new product.
+     */
+    public function update(Request $request, $id) {
+        $product = Product::find($id);
+
+        $product->name      = $request->name;
+        $product->price     = $request->price;
+        $product->category  = $request->category;
+
+        $product->save();
+
+        return response()->json($product);
+    }
+    /**
+     * delete a product.
+     */
+    public function delete($id) {
+        $product = Product::find($id);
+        $product->delete();
+
+        return response()->json('Product successfully deleted!');
+    }
 }
